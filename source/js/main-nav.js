2f495698522fc21c95ba741385cb666639c5e4a6
Дарья <yu.dar@yandex.ru>
2020-04-29 14:41:44 +0300

    Адаптивная верстка главной страницы

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -2,7 +2,6 @@ var header = document.querySelector(".page-header");
 var topMenu = document.querySelector(".page-header__top-menu");
 var navMain = document.querySelector(".main-nav");
 var navToggle = document.querySelector(".page-header__toggle");
-var titleMobile = document.querySelector(".mobile-app__title");
 
 header.classList.remove("page-header--nojs");
 
@@ -11,11 +10,8 @@ navToggle.addEventListener("click", function() {
   if (navToggle.classList.contains("page-header__toggle--close")) {
     navMain.classList.remove("main-nav--closed");
     topMenu.classList.add("page-header__top-menu--active");
-    titleMobile.classList.add("mobile-app__title--notop");
-    titleUserPhoto.classList.add("user-photos__title--notop");
   } else {
     navMain.classList.add("main-nav--closed");
     topMenu.classList.remove("page-header__top-menu--active");
-    titleMobile.classList.remove("mobile-app__title--notop");
   }
 });
