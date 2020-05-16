57d63f168e413ab054e8831a9296d93cf841af92
Дарья <yu.dar@yandex.ru>
2020-05-16 12:25:13 +0300

    Исправляет ошибки

--- source/js/popup.js
+++ source/js/popup.js
@@ -1,27 +1,27 @@
 var buttonSubmit = document.querySelector(".form__submit");
 // var form = document.querySelector(".form");
 
-var popap = document.querySelector(".popap");
-var popapMistake = document.querySelector(".popap--mistake");
-var popapSubmit = document.querySelector(".popap--submit");
-var popapBtnSubmit = document.querySelector(".popap__btn--submit");
-var popapBtnMistake = document.querySelector(".popap__btn--mistake");
+var popap = document.querySelector(".popup");
+var popapMistake = document.querySelector(".popup--mistake");
+var popapSubmit = document.querySelector(".popup--submit");
+var popapBtnSubmit = document.querySelector(".popup__btn--submit");
+var popapBtnMistake = document.querySelector(".popup__btn--mistake");
 var surname = document.querySelector(".surname");
 var name = document.querySelector(".name");
 var email = document.querySelector(".email");
 
 buttonSubmit.addEventListener("click", function () {
   if (!surname.value || !name.value || !email.value) {
-    popapMistake.classList.remove("popap--none");
+    popapMistake.classList.remove("popup--none");
   } else {
-    popapSubmit.classList.remove("popap--none");
+    popapSubmit.classList.remove("popup--none");
   }
 });
 
 popapBtnSubmit.addEventListener("click", function () {
-  popapSubmit.classList.add("popap--none");
+  popapSubmit.classList.add("popup--none");
 });
 
 popapBtnMistake.addEventListener("click", function () {
-  popapMistake.classList.add("popap--none");
+  popapMistake.classList.add("popup--none");
 });
